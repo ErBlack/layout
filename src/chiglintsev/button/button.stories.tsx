c385ab5259f7fb069eee4310872799848b6aae95
Ivan Chiglintsev <chiglincevivan@yandex.ru>
2023-01-25 18:55:28 +0100

    'nice'

---
+++ src/chiglintsev/button/button.stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, Story } from '@storybook/react';
+import React from 'react';
+
+import { Button, ButtonProps } from './button';
+
+export default {
+    title: 'chiglintsev/Button',
+    component: Button,
+    args: {
+        children: "Button",
+        onClick: () => {console.log('yep')}
+    },
+    argTypes: {},
+} as Meta<ButtonProps>;
+
+export const Common: Story<ButtonProps> = (args) => <Button {...args} />;
